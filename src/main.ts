81accef021c951e3456b9248cd48f64e7866767f
Ben-Zama <benzamasimon@gmail.com>
2026-02-22 21:52:46 +0100

    Second build fix attempt

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "./style.scss";
-import { App } from "./App.vue";
+import App from "./App.vue";
 
-import { Vue3Marquee } from "vue3-marquee";
+import Vue3Marquee from "vue3-marquee";
 
 createApp(App).use(Vue3Marquee, { name: "Marquee" }).mount("#app");
